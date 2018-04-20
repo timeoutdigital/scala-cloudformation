8e30014fe3aa5383b6e87b114701e57ab7fb0742
Adam Johnson <me@adamj.eu>
2018-04-20 17:19:02 +0100

    Remove all code
    This repo is dead, removing the code in this commit so that it doesn't show up in search.
    
    JIRA Ticket: INFRA-252
    
    Test Plan: `ls -alh` shows no files except the `.git` directory.

--- cf/src/main/scala/com/timeout/scalacloudformation/Condition.scala
+++
@@ -1,4 +0,0 @@
-package com.timeout.scalacloudformation
-
-case class Condition(logicalId: String,
-                     value: CfExp[Boolean]) extends HasLogicalId
--- cf/src/main/scala/com/timeout/scalacloudformation/Output.scala
+++
@@ -1,7 +0,0 @@
-package com.timeout.scalacloudformation
-
-case class Output(logicalId: String,
-                  Value: CfExp[String],
-                  Condition: Option[Condition] = None,
-                  Export: Option[CfExp[String]] = None,
-                  Description: Option[String] = None) extends HasLogicalId
